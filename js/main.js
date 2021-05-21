3a5f52b3c2e59ae4725c98ca64bfb223153d0636
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-05-21 15:57:55 +0100

    menu descrip

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@ export default function principal() {
     const imagens = document.querySelectorAll('.animais-lista img');
     const descricao = document.querySelectorAll('.animais-descricao section');
 
-    descricao[0].classList.add('ativo', 'show-right');
+    descricao[0].classList.add('ativo', 'show-right')
 
-    function mostraDescricao(index) {
+    function openDescription(index) {
+
         descricao.forEach((item) => {
             item.classList.remove('ativo');
-        });
-        const data = descricao[index].dataset.anime;
-        descricao[index].classList.add('ativo', data);
+        })
+
+        const direcao = descricao[index].dataset.anime;
+        descricao[index].classList.add('ativo', direcao);
     }
     imagens.forEach((img, index) => {
         img.addEventListener('click', () => {
-            mostraDescricao(index);
+            openDescription(index);
         });
-    })
+    });
+
 }
