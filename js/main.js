af8656e9ba9f64523b80dfbc0b2ff3f0644503ae
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-05-21 02:08:40 +0100

    show

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ export default function principal() {
     const imagens = document.querySelectorAll('.animais-lista img');
     const descricao = document.querySelectorAll('.animais-descricao section');
 
+    descricao[0].classList.add('ativo', 'show-right');
+
     function mostraDescricao(index) {
-
-        
-         descricao[index].classList.add('ativo')
-
+        descricao.forEach((item) => {
+            item.classList.remove('ativo');
+        });
+        const data = descricao[index].dataset.anime;
+        descricao[index].classList.add('ativo', data);
     }
-
     imagens.forEach((img, index) => {
         img.addEventListener('click', () => {
             mostraDescricao(index);
