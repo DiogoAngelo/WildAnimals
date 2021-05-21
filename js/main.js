8d3e6bb1c4ab8f881697277eed402a14fdae77e6
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-05-21 01:54:49 +0100

    ulu

--- js/main.js
+++ js/main.js
@@ -2,17 +2,16 @@ export default function principal() {
     const imagens = document.querySelectorAll('.animais-lista img');
     const descricao = document.querySelectorAll('.animais-descricao section');
 
-    descricao[0].classList.add('show-right');
-    descricao[0].classList.add('ativo');
+    function mostraDescricao(index) {
 
-    imagens.forEach((imagem, index) => { 
-        imagem.addEventListener('click', () => {
-            descricao.forEach((item) => {
-                item.classList.remove('ativo')
-            })
-            const anima = descricao[index].dataset.anime;
-            descricao[index].classList.add('ativo', anima);
+        
+         descricao[index].classList.add('ativo')
 
-        })
+    }
+
+    imagens.forEach((img, index) => {
+        img.addEventListener('click', () => {
+            mostraDescricao(index);
+        });
     })
 }
