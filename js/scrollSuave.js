8a71df2c19f9d2dd2b88b84f8fc76287f684577e
Diogo Santos <diogosantos@MBP-de-Diogo.Home>
2021-05-22 21:07:39 +0100

    sapatinho

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -1,18 +1,16 @@
 export default function initScrollSuave() {
     const menuLinks = document.querySelectorAll('a[href^="#"]');
 
-    function suave(event) {
-        event.preventDefault();
-        const href = event.target.getAttribute("href");
-        const select = document.querySelector(href)
-        
-        select.scrollIntoView({
-            behavior: "smooth",
-        }); 
-    }
-    menuLinks.forEach((link) => { 
-        link.addEventListener('click', suave);
-    });
+    function scrola(event) {
+        const href = event.target.getAttribute('href');
+        const link = document.querySelector(href);
 
+        link.scrollIntoView({
+            behavior: "smooth",
+        });
+    }
+    menuLinks.forEach((link) => {
+        link.addEventListener('click', scrola);
+    });
 }
 
